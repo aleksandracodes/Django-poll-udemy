eb9054576a7116bdfdc9749e775f9faf25c9de86
Aleksandra Haniok <a.haniok@gmail.com>
2022-03-22 10:18:26 +0000

    Display the related objects in a compact, table-based format in admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from .models import Choice, Question
 
+
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3  # add extra 3 choices by default
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields':['question_text']}),
-        ('Date Information', {'fields':['pub_date']}),
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
-
-
