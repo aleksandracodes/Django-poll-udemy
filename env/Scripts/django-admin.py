a295ffa3fb573304f6ea7150cebddf396b3b86c0
Aleksandra Haniok <a.haniok@gmail.com>
2022-03-22 08:34:34 +0000

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Aleksandra\Documents\Programming\Udemy and Skillshare courses\Django\Django-Udemy\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ mysite/polls/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+
+# Create a new table in our db which will hold our questions
+# and each of a question will have a text & publish date
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date_published")
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
